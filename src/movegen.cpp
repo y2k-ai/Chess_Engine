9880108eaf3591545e77b38c3c92b80131b635d7
Chess Engine Developer <prod.r9hzmxnmd49j@gmail.com>
2026-04-19 19:04:56 -0300

    fix: use pawn_attacks table in is_attacked, raise capture buffer to 320

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -6,11 +6,7 @@ int king_sq(const Board& b, Color c) {
 }
 
 bool is_attacked(int sq, Color by, Bitboard occ, const Board& b) {
-    Bitboard bb = sq_bb(sq);
-    Bitboard pawn_atk = (by == WHITE)
-        ? ((bb >> 9) & ~FILE_H) | ((bb >> 7) & ~FILE_A)
-        : ((bb << 7) & ~FILE_H) | ((bb << 9) & ~FILE_A);
-    return (pawn_atk                              & b.pieces[by][PAWN])   ||
+    return (Magic::pawn_attacks[by][sq]           & b.pieces[by][PAWN])   ||
            (Magic::knight_attacks[sq]             & b.pieces[by][KNIGHT]) ||
            (Magic::king_attacks[sq]               & b.pieces[by][KING])   ||
            (Magic::bishop_attacks(sq, occ)        & (b.pieces[by][BISHOP] | b.pieces[by][QUEEN])) ||
@@ -188,7 +184,7 @@ int generate_moves(const Board& b, Move* list) {
 }
 
 int generate_captures(const Board& b, Move* list) {
-    Move all[256];
+    Move all[320];
     int n = generate_moves(b, all);
     Move* start = list;
     for (int i = 0; i < n; i++)
